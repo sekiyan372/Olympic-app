5c09f80d2f0f03e6d7ae6733cbfbcc8f57d78acf
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2020-02-26 00:10:34 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <div class="title">ぼくのかんがえたさいきょうのさむらいじゃぱん</div>
+  </section>
+</template>
+
+<script>
+  import "@/assets/css/index.css"
+  import draggable from 'vuedraggable'
+
+  export default {
+    components: {}
+  }
+</script>
